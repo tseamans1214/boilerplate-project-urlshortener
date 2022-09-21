f16277dcbbeed9f71573d9a8201cffa6ee5731bd
Tommy Seamans <tlseamans1214@gmail.com>
2022-09-21 15:58:40 -0700

    Fix error checking by using hostname

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const dns = require('dns');
+const options = {
+  all:true,
+};
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -27,13 +32,23 @@ app.get('/api/hello', function(req, res) {
 
 var urlArray = [];
 app.post('/api/shorturl', function(req, res) {
-  urlArray.push(req.body.url_input);
-  res.json({"original_url": req.body.url,
-            "short_url": urlArray.length });
+  let url = new URL(req.body.url);
+  dns.lookup(url.hostname, options, (err, address) => {
+    //console.log('address: %j family: IPv%s', address, family);
+    if (err) {
+      res.json({"error": "invalid url" });
+    } else {
+      urlArray.push(req.body.url);
+      res.json({"original_url": req.body.url,
+              "short_url": urlArray.length });
+    }
+  });
+  
 });
 
-app.get('/api/shortulr/:url?', function(req, res) {
-  res.send(urlArray(req.params.url-1));
+app.get('/api/shorturl/:url?', function(req, res) {
+  //res.redirect("google.com");
+  res.redirect("" + urlArray[req.params.url-1]);
 });
 
 app.listen(port, function() {
